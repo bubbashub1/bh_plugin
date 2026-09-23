0507e1c4dc38a7806c62c1cb5d4e388202b1a81d
bubbashub1 <bubbashub1@gmail.com>
2026-09-23 07:42:59 +0100

    Hide Directorist map filters

--- includes/class-bh-directory.php
+++ includes/class-bh-directory.php
@@ -160,7 +160,7 @@ final class Directory {
         }
 
         $output = do_shortcode(sprintf(
-            '[directorist_all_listing view="map" ids="%s" header="no" show_pagination="no" listings_per_page="%d"]',
+            '[directorist_all_listing view="map" ids="%s" header="no" advanced_filter="no" show_pagination="no" listings_per_page="%d"]',
             esc_attr(implode(',', $ids)),
             count($ids)
         ));
