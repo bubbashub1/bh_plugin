114df3f10270ffdde0bd1e76fa57caf9cd9febd7
bubbashub1 <bubbashub1@gmail.com>
2026-09-23 13:14:58 +0100

    Update saved searches page URL to /my-hub/saved-searches

--- includes/class-bh-directory-search.php
+++ includes/class-bh-directory-search.php
@@ -78,8 +78,8 @@ final class DirectorySearch {
     }
 
     public static function saved_searches_url(): string {
-        $page = get_page_by_path('saved-searches');
-        return $page instanceof \WP_Post ? get_permalink($page) : home_url('/saved-searches/');
+        $page = get_page_by_path('my-hub/saved-searches');
+        return $page instanceof \WP_Post ? get_permalink($page) : home_url('/my-hub/saved-searches/');
     }
 
     public static function handle_delete_saved_search(): void {
